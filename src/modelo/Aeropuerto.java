68204ae05d2d07270c026f1d447a7c4526a90fe1
cubas <cubas@192.168.1.22>
2022-01-26 19:27:03 -0500

    Segundo Commit

---
+++ src/modelo/Aeropuerto.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author cubas
+ */
+public class Aeropuerto {
+    private String nombre;
+    private String direccion;
+    private boolean estado;
+
+    public Aeropuerto(String nombre, String direccion, boolean estado) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Aeropuerto(" + "nombre:" + nombre + " direccion:" + direccion + " estado:" + estado + ')';
+    }
+    
+    
+}
---
+++ src/modelo/Pais.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author cubas
+ */
+public class Pais {
+    private String codigo;
+    private String nombre;
+    private Ciudad ciudad;
+
+    public Pais(String codigo, String nombre, Ciudad ciudad) {  
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+    
+}
